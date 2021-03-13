aa5e8a62854c1bb1dd0f05e3bef6e405b31535df
Marzieh Bahri <marzieh@pragmateam.dev>
2021-03-12 20:17:38 -0700

    modified rowShuffle method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,14 +36,7 @@ function App() {
   function randomize(end) {
     return Math.floor(Math.random() * Math.floor(end));
   }
-
-  function columnShuffle() {
-    const start = randomize(5);
-    const indexArr = [];
-    let i;
-    for (i = 0; i < 5; i++) {
-      indexArr[i] = start + i * 5;
-    }
+  function fillList(indexArr) {
     const newList = list.map((item, index) => {
       let fruitIndex;
       if (indexArr.indexOf(index) > -1) {
@@ -61,9 +54,31 @@ function App() {
     });
     setList(newList);
   }
+  function columnShuffle() {
+    const start = randomize(5);
+    const indexArr = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+      indexArr[i] = start + i * 5;
+    }
+    fillList(indexArr);
+  }
   function rowShuffle() {
+    /*
+     * row number 0 => 00 01 02 03 04
+     * row number 1 => 05 06 07 08 09
+     * row number 2 => 10 11 12 13 14
+     * row number 3 => 15 16 17 18 19
+     * row number 4 => 20 21 22 23 24
+     *
+     * */
     let rowNumber = randomize(5);
-
+    const indexArr = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+      indexArr[i] = rowNumber * 5 + i;
+    }
+    fillList(indexArr);
   }
   function diagnolShuffle() {}
 
