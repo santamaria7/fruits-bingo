432b5f3a78099d575f6dd05ff0fcc8e24e3014db
Marzieh Bahri <marzieh@pragmateam.dev>
2021-03-13 21:39:00 -0700

    moved shuffle functions to utils as they are not component-specific functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,26 +2,22 @@ import "./App.scss";
 import React, { useEffect, useMemo, useState } from "react";
 import Button from "../Button";
 import { fruits, randomText } from "../../constants";
+import {chooseWord, columnShuffle, diagonalShuffle, multipleShuffle, randomize, rowShuffle} from "../../utils";
 
 function App() {
   const [list, setList] = useState([]);
   const [selectionCount, setSelectionCount] = useState(0);
   const [wrongSelection, setWrongSelection] = useState(0);
   const [winning, setWinning] = useState(false);
-  function randomize(end) {
-    return Math.floor(Math.random() * Math.floor(end));
-  }
 
-  function chooseWord(newList, reference, repeat) {
-    let j = randomize(25);
-    if (!repeat) {
-      while (newList.indexOf(reference[j]) > -1) {
-        j = randomize(25);
-      }
-    }
-
-    return reference[j];
-  }
+  const shuffleMethods = useMemo(() => {
+    return {
+      0: columnShuffle,
+      1: rowShuffle,
+      2: diagonalShuffle,
+      3: multipleShuffle,
+    };
+  }, []);
 
   function fillList(indexArr, repeat) {
     const newList = [];
@@ -31,85 +27,20 @@ function App() {
         // The free slot
         newList[i] = "";
       } else {
-        newList[i] =
-          indexArr.indexOf(i) > -1
-            ? chooseWord(newList, fruits, repeat) // choose randomly from fruits array
-            : chooseWord(newList, randomText, repeat); //choose randomly from words array
+        const reference = indexArr.indexOf(i) > -1 ? fruits : randomText;
+        newList[i] = chooseWord(newList, reference, repeat);
       }
     }
     setList(newList);
   }
-  function columnShuffle() {
-    const start = randomize(5);
-    const indexArr = [];
-    let i;
-    for (i = 0; i < 5; i++) {
-      indexArr[i] = start + i * 5;
-    }
-    return indexArr;
-  }
-  function rowShuffle() {
-    /*
-     * row number 0 => 00 01 02 03 04
-     * row number 1 => 05 06 07 08 09
-     * row number 2 => 10 11 12 13 14
-     * row number 3 => 15 16 17 18 19
-     * row number 4 => 20 21 22 23 24
-     *
-     * */
-    let rowNumber = randomize(5);
-    const indexArr = [];
-    let i;
-    for (i = 0; i < 5; i++) {
-      indexArr[i] = rowNumber * 5 + i;
-    }
-    return indexArr;
-  }
-  function diagonalShuffle() {
-    /*
-     * row number 0 => 00 01 02 03 04
-     * row number 1 => 05 06 07 08 09
-     * row number 2 => 10 11 12 13 14
-     * row number 3 => 15 16 17 18 19
-     * row number 4 => 20 21 22 23 24
-     *
-     * */
-    let i;
-    const indexArr = [];
-    const side = randomize(2); // 0 or 1
-    if (side === 0) {
-      // from left to right \
-      for (i = 0; i < 5; i++) {
-        indexArr[i] = 6 * i; //  5 * i + i
-      }
-    } else {
-      //from right to left /
-      for (i = 5; i > 0; i--) {
-        indexArr[i] = 4 * i; // i * 5 - i
-      }
-    }
-    return indexArr;
-  }
-  function multipleShuffle() {
-    // The simplest case of having 1 row, 1 column and 1 diagonal bingo
-    const indexArr1 = rowShuffle();
-    const indexArr2 = columnShuffle();
-    const indexArr3 = diagonalShuffle();
-    return [...new Set([...indexArr1, ...indexArr2, ...indexArr3])];
-  }
-  const shuffleMethods = useMemo(() => {
-    return {
-      0: columnShuffle,
-      1: rowShuffle,
-      2: diagonalShuffle,
-      3: multipleShuffle,
-    };
-  }, []);
+
   function shuffle() {
-    const selected = randomize(4); // expected output: 0, 1, 2 or 3
+    const selected = 3; // randomize(4); // expected output: 0, 1, 2 or 3
     const indexArr = shuffleMethods[selected]();
+
     fillList(indexArr, selected === 3);
   }
+
   function selectItem(item, deselect) {
     if (fruits.indexOf(item) > -1) {
       setSelectionCount((prevState) => prevState + 1);
@@ -122,15 +53,16 @@ function App() {
       });
     }
   }
+
   async function celebrate() {
     setWinning(true);
-    setSelectionCount(0);
     await setWrongSelection(0);
     window.setTimeout(() => {
       setWinning(false);
       //shuffle();
     }, 3000);
   }
+
   useEffect(() => {
     shuffle();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -140,6 +72,7 @@ function App() {
       celebrate();
     }
   }, [selectionCount, wrongSelection]);
+
   return (
     <div className="App">
       <h1>Fruit Bingo</h1>
---
+++ src/utils.js
@@ -0,0 +1,75 @@
+
+export function randomize(end) {
+    return Math.floor(Math.random() * Math.floor(end));
+}
+
+export function chooseWord(newList, reference, repeat) {
+    let j = randomize(25);
+    if (!repeat) {
+        while (newList.indexOf(reference[j]) > -1) {
+            j = randomize(25);
+        }
+    }
+
+    return reference[j];
+}
+
+export function columnShuffle() {
+    const start = randomize(5);
+    const indexArr = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+        indexArr[i] = start + i * 5;
+    }
+    return indexArr;
+}
+export function rowShuffle() {
+    /*
+     * row number 0 => 00 01 02 03 04
+     * row number 1 => 05 06 07 08 09
+     * row number 2 => 10 11 12 13 14
+     * row number 3 => 15 16 17 18 19
+     * row number 4 => 20 21 22 23 24
+     *
+     * */
+    let rowNumber = randomize(5);
+    const indexArr = [];
+    let i;
+    for (i = 0; i < 5; i++) {
+        indexArr[i] = rowNumber * 5 + i;
+    }
+    return indexArr;
+}
+export function diagonalShuffle() {
+    /*
+     * row number 0 => 00 01 02 03 04
+     * row number 1 => 05 06 07 08 09
+     * row number 2 => 10 11 12 13 14
+     * row number 3 => 15 16 17 18 19
+     * row number 4 => 20 21 22 23 24
+     *
+     * */
+    let i;
+    const indexArr = [];
+    const side = randomize(2); // 0 or 1
+    if (side === 0) {
+        // from left to right \
+        for (i = 0; i < 5; i++) {
+            indexArr[i] = 6 * i; //  5 * i + i
+        }
+    } else {
+        //from right to left /
+        for (i = 5; i > 0; i--) {
+            indexArr[i] = 4 * i; // i * 5 - i
+        }
+    }
+    return indexArr;
+}
+export function multipleShuffle() {
+    // The simplest case of having 1 row, 1 column and 1 diagonal bingo
+    const indexArr1 = rowShuffle();
+    const indexArr2 = columnShuffle();
+    const indexArr3 = diagonalShuffle();
+    return [...new Set([...indexArr1, ...indexArr2, ...indexArr3])];
+}
+
